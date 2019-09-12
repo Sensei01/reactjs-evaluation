9601971ad234279e62da31917ec6167e2c76a0b7
Sensei01 <ahmadsanibarde@gmail.com>
2019-09-12 22:13:59 +0100

    Initial commit

---
+++ src/components/QuizData.js
@@ -0,0 +1,26 @@
+export const QuizData = [
+  {
+    id: 0,
+    question: ` The Thrust Leader understands the topic`,
+    options: [`Very Much`, `Kind Of`, `Not At All`],
+    answer: `Very Much`
+  },
+  {
+    id: 1,
+    question: `The Thrust Leader shows respect towards students and encourages class participation`,
+    options: [`A lot`, `Sometimes`, `Never`],
+    answer: `Never`
+  },
+  {
+    id: 3,
+    question: `Today's class was Boring`,
+    options: [`yup!`, `Kind of`, `Nahhh`],
+    answer: `yup!`
+  },
+  {
+    id: 4,
+    question: `I learned some things from today's Lesson`,
+    options: [`I really did`, `Kind Of`, `Nothing`],
+    answer: `I really did`
+  }
+];
---
+++ src/components/testing.js
@@ -0,0 +1,11 @@
+// <div>
+//  <h2>Thank You!!! Your Feedback Has Been Submitted</h2>
+//  <p>selected options are:</p>
+//  <ol>
+//    {QuizData.map((item, index) => (
+//     <li className="ui floating message options" key={index}>
+//       {item.answer}
+//       </li>
+//    ))}
+//   </ol>
+//   </div>
