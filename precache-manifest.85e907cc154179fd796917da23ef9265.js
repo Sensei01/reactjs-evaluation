7a5416c0acc8da48c9801b60fb0b8270cdaa64d4
Sensei01 <ahmadsanibarde@gmail.com>
2019-09-13 00:54:12 +0100

    Updates

---
+++ precache-manifest.85e907cc154179fd796917da23ef9265.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3eff6438e0bfd7b7ae46a702b507b83",
+    "url": "/reactjs-evaluation/index.html"
+  },
+  {
+    "revision": "3c3fe9886355d698eeb1",
+    "url": "/reactjs-evaluation/static/css/main.a582b1a1.chunk.css"
+  },
+  {
+    "revision": "b001cbee59c1fe6ce21b",
+    "url": "/reactjs-evaluation/static/js/2.9dfbfa7b.chunk.js"
+  },
+  {
+    "revision": "3c3fe9886355d698eeb1",
+    "url": "/reactjs-evaluation/static/js/main.789b24bf.chunk.js"
+  },
+  {
+    "revision": "54136b67b62fc74e0845",
+    "url": "/reactjs-evaluation/static/js/runtime~main.97948fed.js"
+  }
+]);
