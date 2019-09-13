7589514cf131bc7e4bff3736776dcc7127893d0d
Sensei01 <ahmadsanibarde@gmail.com>
2019-09-13 01:15:50 +0100

    Updates

---
+++ precache-manifest.65b037b9d6c7f5a867b0636a5dbc3770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0388d7ab70eef43a3f025446eaba0de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e19c3e6fca938c85ac2",
+    "url": "/static/css/main.a582b1a1.chunk.css"
+  },
+  {
+    "revision": "b001cbee59c1fe6ce21b",
+    "url": "/static/js/2.9dfbfa7b.chunk.js"
+  },
+  {
+    "revision": "3e19c3e6fca938c85ac2",
+    "url": "/static/js/main.8b9fedf5.chunk.js"
+  },
+  {
+    "revision": "08edf4ae2b645d28ea82",
+    "url": "/static/js/runtime~main.bece8cfc.js"
+  }
+]);
